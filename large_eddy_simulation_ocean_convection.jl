3e5c444a1e108a34f288584bfed1662e3ba82c78
Gregory Wagner <wagner.greg@gmail.com>
2022-02-25 07:08:14 -0700

    Update LES parameters

--- large_eddy_simulation_ocean_convection.jl
+++ large_eddy_simulation_ocean_convection.jl
@@ -24,12 +24,12 @@ model = NonhydrostaticModel(; grid, coriolis, closure, boundary_conditions, trac
                               timestepper = :RungeKutta3, advection = WENO5(),
                               buoyancy = BuoyancyTracer())
                           
-bᵢ(x, y, z) = 1e-5 * z
+bᵢ(x, y, z) = 1e-6 * z
 ϵ(x, y, z) = 1e-6 * randn() # noise
 set!(model, b=bᵢ, u=ϵ, v=ϵ, w=ϵ)
 
 # Time-stepping
-simulation = Simulation(model, Δt=10minutes, stop_time=24hours)
+simulation = Simulation(model, Δt=10minutes, stop_time=12hours)
 
 progress(sim) = @printf("Iter: %d, time: %s, Δt: %s, max|w|: %s\n",
                         iteration(sim), prettytime(sim), prettytime(sim.Δt), maximum(abs, model.velocities.w))
@@ -54,20 +54,30 @@ run!(simulation)
 
 data_path = simulation.output_writers[:fields].filepath
 e = FieldTimeSeries(data_path, "e")
+b = FieldTimeSeries(data_path, "b")
 x, y, z = nodes(e)
 Nt = length(e.times)
 
-fig = Figure()
-ax = Axis(fig[1, 1])
-slider = Slider(fig[2, 1], range=1:Nt, startvalue=1)
+fig = Figure(resolution=(1600, 800))
+axe = Axis(fig[1, 1])
+axξ = Axis(fig[1, 2])
+slider = Slider(fig[3, :], range=1:Nt, startvalue=1)
 n = slider.value
 
 eⁿ = @lift dropdims(interior(e[$n]), dims=2)
-hm = heatmap!(ax, x, z, eⁿ; colormap=:solar, colorrange=(0, maximum(e) / 2))
-Colorbar(fig[1, 2], hm)
+bⁿ = @lift dropdims(interior(b[$n]), dims=2)
+hm = heatmap!(axe, x, z, eⁿ; colormap=:solar, colorrange=(0, maximum(e) / 4))
+Colorbar(fig[2, 1], hm, vertical=false, flipaxis=false, label="Turbulent kinetic energy (m² s⁻²)")
 
-title = @lift "Turbulent kinetic energy at t = " * prettytime(e.times[$n])
+hm = heatmap!(axξ, x, z, bⁿ; colormap=:deep, colorrange=(minimum(b), maximum(b)))
+Colorbar(fig[2, 2], hm, vertical=false, flipaxis=false, label="Buoyancy (m s⁻²)")
+
+title = @lift "Idealized ocean convection at t = " * prettytime(e.times[$n])
 Label(fig[0, :], title)
 
 display(fig)
 
+#record(fig, "idealized_ocean_convection.mp4", 1:Nt, framerate=12) do nn
+#    @info "Rendering frame $nn of $Nt...
+#    n[] = nn
+#end
